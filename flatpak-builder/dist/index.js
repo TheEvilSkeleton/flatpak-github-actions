9565be558778fd71cb9620ceeca2b541b65d4a0d
Hari Rana <theevilskeleton@riseup.net>
2025-01-03 14:25:35 -0500

    Implicitly set remote

--- flatpak-builder/dist/index.js
+++ flatpak-builder/dist/index.js
@@ -227,7 +227,7 @@ const build = async (manifest, manifestPath, cacheHitKey, config) => {
   const args = [
     `--repo=${config.localRepoName}`,
     '--disable-rofiles-fuse',
-    `--install-deps-from=${config.repositoryName}`,
+    // `--install-deps-from=${config.repositoryName}`,
     '--force-clean',
     `--default-branch=${branch}`,
     `--arch=${config.arch}`
